c69d11c18a6ff2b1ba8ca98835baf8524df217b8
Peter Kim <seongjooon@gmail.com>
2018-11-03 20:13:49 +0900

    응응?

--- level1/watermelon.js
+++ level1/watermelon.js
@@ -26,4 +26,4 @@ console.log(makeWatermelon(watermelonNum))
 
 const waterMelon = n => "수박".repeat(n).slice(0,n);
 
-console.log(waterMelon(watermelonNum))
+console.log(makeWatermelon(watermelonNum))
