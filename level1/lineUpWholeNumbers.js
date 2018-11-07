64f7850577416607e96b111668356cf2dfa785fb
Peter Kim <seongjooon@gmail.com>
2018-11-07 15:37:39 +0900

    정수사이의 합

--- level1/lineUpWholeNumbers.js
+++ level1/lineUpWholeNumbers.js
@@ -23,3 +23,9 @@ const lineUpNum = (n) => {
     return splitedNum
 }
 console.log(lineUpNum(n))
+
+
+function solution(n) {
+    // 문자풀이
+    return parseInt((n+"").split("").sort().reverse().join(""));
+}
