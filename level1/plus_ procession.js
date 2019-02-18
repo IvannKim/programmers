e9dab21d2fc5c29a115fad09213dc374bac8936e
Peter Kim <seongjooon@gmail.com>
2019-02-18 21:36:47 +0900

    completed mission what is a plus_procession

--- level1/plus_ procession.js
+++ level1/plus_ procession.js
@@ -1,6 +1,7 @@
 /*
 문제 설명
-행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 
+2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
 
 제한 조건
 행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
@@ -10,42 +11,26 @@ arr1	arr2	return
 [[1],[2]]	[[3],[4]]	[[4],[6]]
 */
 
-const arr1 = [1, 2];
-const arr2 = [3, 4];
+const arr3 = [[1, 2], [2, 3]];
+const arr4 = [[3, 4], [5, 6]];
 
-function solution(arr1, arr2) {
-    const result = [];
-    for (let i = 0; i < arr1.length; i++) {
-        result.push(arr1[i] + arr2[i])
-    }
-    return result
-}
-
-console.log(solution(arr1, arr2))
-
-const arr3 = [[1,2],[2,3]];
-const arr4 = [[3,4],[5,6]];
-
-function solution4(arr3, arr4) {
+function solution(arr3, arr4) {
+    let res = [];
     const result = [];
     for (let i = 0; i < arr3.length; i++) {
-        for (let j = 0; j < arr3[0].length; j++) {
-            result.push(arr3[i][j] + arr4[i][j])
+        for (let j = 0; j < arr3[i].length; j++) {
+            res.push(arr3[i][j] + arr4[i][j]);
         }
+        result.push(res)
+        res = [];
     }
     return result
 }
 
-console.log(solution4(arr3, arr4))
+console.log(solution(arr3, arr4))
 
-function solution3(arr3, arr4) {
-    return [arr3[0][0] + arr4[0][0], arr3[1][0] + arr4[1][0]]
+function solution1(A, B) {
+    return A.map((a, i) => a.map((b, j) => b + B[i][j]));
 }
 
-console.log(solution3(arr3, arr4))
-
-function solution2(arr3, arr4) {
-    return [[arr3[0][0] + arr4[0][0]], [arr3[1][0] + arr4[1][0]]]
-}
-
-console.log(solution2(arr3, arr4))
+console.log(solution1(arr3, arr4))
