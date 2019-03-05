d394031ea748768193b5a7585abf0a23f72d8742
Peter Kim <seongjooon@gmail.com>
2019-03-05 17:18:51 +0900

    나의 착각으로인한 하드코딩의 결과...

--- level1/(o?)string_arr.js
+++ level1/(o?)string_arr.js
@@ -1,31 +1,22 @@
-/*
-문자열 내림차순으로 배치하기
-
+/*문자열 내림차순으로 배치하기
 문제 설명
 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
 s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
-
 제한 사항
 str은 길이 1 이상인 문자열입니다.
 입출력 예
 s	return
 Zbcdefg	gfedcbZ*/
 
-const s = "Zbcdefg"
+const s = 'Zbcdefg'
 
-// const solution = s => s.split('').reverse().join('')
+const solution = s => s.split('').sort().reverse().join('')
 
 console.log(solution(s))
 
-// function solution(s) {
-//     const sArray = s.split('')
-//     const lowerS = s.toLowerCase().split('')
-//     console.log(lowerS)
-//     const sortedS = lowerS.sort()
-//     console.log(sortedS)
+// function solution2(s) {
+//     const sortedArray = s.split('').sort();
+//     const lowerCase = sortedArray.filter(n => n >= String.fromCharCode('97') && n <= String.fromCharCode('122')).reverse().join('')
+//     const upperCase = sortedArray.filter(n => n >= String.fromCharCode('65') && n <= String.fromCharCode('90')).reverse().join('')
+//     return lowerCase+upperCase
 // }
-
-function solution(s) {
-    const reversedS = s.split('').reverse()
-    return reversedS.join('')
-}
